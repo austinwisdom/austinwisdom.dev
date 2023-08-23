ea7fbed375bd63b820008b3c75adb3ead81ebb20
Austin <ataraxia@Austins-MacBook-Air.local>
2023-08-23 15:04:07 -0300

    Finished homepage 1.0

--- austinwisdom.dev/tailwind.config.js
+++ austinwisdom.dev/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 0 .75rem rgba(255, 0, 27, 1.5)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
+      backgroundImage: {
+        'aw-logo': "url('./assets/images/logo/aw.svg')"
+      }
+    },
   },
   plugins: [],
 }
@@ -17,5 +28,13 @@ export default {
 //   'lightblack': '#1f1f1f',
 //   'black': '#181818',
 //   'red': '#ff001b',
+// }
+
+// dropShadow: {
+//   '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+//   '4xl': [
+//       '0 35px 35px rgba(0, 0, 0, 0.25)',
+//       '0 45px 65px rgba(0, 0, 0, 0.15)'
+//   ]
 // }
 
