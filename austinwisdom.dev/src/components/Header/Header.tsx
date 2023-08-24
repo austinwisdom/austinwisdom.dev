00ea7871726b37c86e66cd0278ed00dbf0e228f7
Austin <ataraxia@Austins-MacBook-Air.local>
2023-08-24 13:42:21 -0300

    Finished homepage and started about page.

--- austinwisdom.dev/src/components/Header/Header.tsx
+++ austinwisdom.dev/src/components/Header/Header.tsx
@@ -1,22 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
     return (
-        <nav className='justify-end flex flex-row bg-red-100'>
+        <nav className='justify-center sm:justify-end flex flex-row bg-red-100'>
         {/* <img className='h-16' src={awLogo} /> */}
         <ul className='bg-neutral-950 rounded-2xl w-1/2 mx-7 h-10 px-4 flex flex-row content-end my-5'>
           <li className='list-none text-red-100 mx-4 self-center'>
-            <a className='transition hover:drop-shadow-3xl cursor-pointer'>home
+            <NavLink to={"/"}  className='transition hover:drop-shadow-3xl cursor-pointer'>home
               <span className="absolute inset-x-1 -bottom-px h-px w-full bg-gradient-to-r from red-500/0 via-red-500/40 to-red-500/0 dark:from-red-400/0 dark:via-red-400/40 dark:to-red-400/0"></span>
-            </a>
+            </NavLink>
           </li>
           <li className='list-none text-red-100 mx-4 self-center'>
-            <a className='transition hover:drop-shadow-3xl cursor-pointer'>about
+            <NavLink to={"/about"} className='transition hover:drop-shadow-3xl cursor-pointer'>about
               <span className="absolute inset-x-1 -bottom-px h-px w-full bg-gradient-to-r from red-500/0 via-red-500/40 to-red-500/0 dark:from-red-400/0 dark:via-red-400/40 dark:to-red-400/0"></span>
-            </a>
+            </NavLink>
           </li>
           <li className='list-none text-red-100 mx-4 self-center'>
-            <a className='transition hover:drop-shadow-3xl cursor-pointer'>projects
+            <NavLink to={"/projects"} className='transition hover:drop-shadow-3xl cursor-pointer'>projects
               <span className="absolute inset-x-1 -bottom-px h-px w-full bg-gradient-to-r from red-500/0 via-red-500/40 to-red-500/0 dark:from-red-400/0 dark:via-red-400/40 dark:to-red-400/0"></span>
-            </a>
+            </NavLink>
           </li>
         </ul>
       </nav>
--- austinwisdom.dev/src/main.tsx
+++ austinwisdom.dev/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import App from './App.tsx'
+import AboutPage from './pages/About/AboutPage.tsx';
 import Header from './components/Header/Header.tsx';
 import './index.css'
 
 export enum ROUTES {
-  HOME = '/'
+  HOME = '/',
+  ABOUT = '/about'
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -16,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Header/>
       <Routes>
         <Route path={ROUTES.HOME} element={<App />}/>
+        <Route path={ROUTES.ABOUT} element={<AboutPage />}/>
       </Routes>
     </Router>
   </React.StrictMode>,
