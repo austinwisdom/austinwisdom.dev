da409f22138fb1a6338111b16114c8670bdb0da4
Austin <ataraxia@Austins-MacBook-Air.local>
2023-08-29 11:46:34 -0300

    Added secret message.

--- austinwisdom.dev/src/components/Footer/Footer.tsx
+++ austinwisdom.dev/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='justify-center sm:justify-start flex flex-row bg-neutral-950'>
         
         <div className="text-red-100 self-center h-full">
-            <p className="text-right px-5 py-5">© Austin Wisdom all rights reserved</p>
+            <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><p className="py-5 px-5 text-right transition hover:text-red-600">© Austin Wisdom all rights reserved</p></a>
         </div>
       </footer>
     );
