2dda4c85fe4b6508095104d2cdf90e10ca9d96ce
austinwisdom <austin.n.wisdom@gmail.com>
2024-02-07 19:08:35 -0300

    Added working sphere on aboutthreejs page.:

--- austinwisdom.dev/src/pages/AboutPageThreejs/AboutPageThreejs.tsx
+++ austinwisdom.dev/src/pages/AboutPageThreejs/AboutPageThreejs.tsx
@@ -6,30 +6,37 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 const AboutPageThreejs = () => {
     const refContainer = useRef(null);
     useEffect(() => {
-      
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      // document.body.appendChild( renderer.domElement );
-      // use ref as a mount point of the Three.js scene instead of the document.body
-      //@ts-ignore
-      refContainer.current && refContainer.current.appendChild( renderer.domElement );
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-      camera.position.z = 5;
-      const animate = function () {
+        
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // document.body.appendChild( renderer.domElement );
+        // use ref as a mount point of the Three.js scene instead of the document.body
+        //@ts-ignore
+        refContainer.current && refContainer.current.appendChild( renderer.domElement );
+        const geometry = new THREE.SphereGeometry(13, 16, 16)
+        const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true})
+        const sphere = new THREE.Mesh(geometry, material)
+
+        scene.add(sphere)
+        camera.position.setZ(30)
+        camera.position.setX(-3)
+        const animate = function () {
         requestAnimationFrame(animate);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        sphere.rotation.x += 0.005
+        sphere.rotation.y += 0.005
+        sphere.rotation.z += 0.005
+        
         renderer.render(scene, camera);
-      };
+        };
+
       animate();
+
     }, []);
+
     return (
-      <div ref={refContainer}></div>
+      <main ref={refContainer}></main>
   
     );
   }
